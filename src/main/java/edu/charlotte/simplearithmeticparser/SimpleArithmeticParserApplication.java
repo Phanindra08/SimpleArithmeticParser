76e3015a01a4a31139515bde03766a285281ad57
Sai Phanindra Kothuri <skothur1@uncc.edu>
2025-10-06 12:17:14 -0400

    Created a Sample Project structure using Spring Initializer

---
+++ src/main/java/edu/charlotte/simplearithmeticparser/SimpleArithmeticParserApplication.java
@@ -0,0 +1,13 @@
+package edu.charlotte.simplearithmeticparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleArithmeticParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleArithmeticParserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/charlotte/simplearithmeticparser/SimpleArithmeticParserApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.charlotte.simplearithmeticparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleArithmeticParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
