bdeb55aed0eda7a9c50c8ee6fe17f7128037df1c
skothur1 <kothuriphanindra1999@gmail.com>
2025-10-18 18:45:07 -0400

    Creating a Parse Tree for the grammar

--- src/main/java/edu/charlotte/simplearithmeticparser/utils/Constants.java
+++ src/main/java/edu/charlotte/simplearithmeticparser/utils/Constants.java
@@ -15,33 +15,13 @@ public final class Constants {
     public static final String ERROR_MESSAGE_FOR_MISSING_JOB_PARAMETERS = "Missing required job parameters. " +
             "Use --job.name=<jobName> and --input.file=<inputFile>";
 
-    public static final String AST_NODE_DL_PROGRAM = "DLProgram";
-    public static final String AST_NODE_DL_FORMULA = "Formula";
-    public static final String AST_NODE_DL_PROGRAM_CONTEXT = "Program";
-    public static final String AST_NODE_DL_BINARY_EXPRESSION = "BinaryExpression";
-    public static final String AST_NODE_DL_TERM_WITH_PARENTHESES = "TermWithParentheses";
-
-    public static final String AST_NODE_REL_DL_PROGRAM = "RelationalDLProgram";
-    public static final String AST_NODE_REL_DL_FORMULA = "Relational Formula";
-    public static final String AST_NODE_REL_DL_PROGRAM_CONTEXT = "Relational Program";
-    public static final String AST_NODE_REL_DL_TERM = "Relational Term";
-
-    public static final char PROGRAM_CONSIDERED_L = 'L';
-    public static final char PROGRAM_CONSIDERED_R = 'R';
-    public static final char PROGRAM_CONSIDERED_G = 'G';
-    public static final String LEFT_PROGRAM = "@L";
-    public static final String RIGHT_PROGRAM = "@R";
-
-    public static final String REL_DL_OPEN_BRACKETS = "(#";
-    public static final String REL_DL_CLOSE_BRACKETS = ")#";
-    public static final String REL_DL_COMMA = ",#";
-    public static final String REL_DL_ASSIGNMENT_OPERATOR = ":=#";
-
-    public static final String DL_ASSIGNMENT_OPERATOR = ":=";
-    public static final String DL_SEMI_COLON = ";";
+    public static final String AST_NODE_SIMPLE_ARITHMETIC_PROGRAM = "SimpleArithmeticProgram";
+    public static final String AST_NODE_SIMPLE_ARITHMETIC_EXPRESSION = "expr";
+    public static final String AST_NODE_SIMPLE_ARITHMETIC_TERM = "term";
+    public static final String AST_NODE_SIMPLE_ARITHMETIC_FACTOR = "factor";
+    public static final String AST_NODE_SIMPLE_ARITHMETIC_NUMBER = "number";
+    public static final String AST_NODE_SIMPLE_ARITHMETIC_DIGIT = "digit";
 
     public static final String AST_GENERATION_PROCESS_SUFFIX = " Ast Generation Process";
-    public static final String KEYMAERAX_OUTPUT_CONVERSION_SUFFIX = " to KeYmaeraX Output Conversion Process";
-
-    public static final String DIFFERENTIAL_DYNAMIC_LOGIC = "Differential Dynamic Logic";
+    public static final String SIMPLE_ARITHMETIC = "Simple Arithmetic";
 }
